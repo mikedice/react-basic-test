46243341ebbf15fe746475a23aea6e571152c41d
Mike Dice <mikedice417@hotmail.com>
2023-11-19 15:24:20 -0800

    update message

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const name = 'Bob';
-  return <div>Hi {name}. You are a boob</div>;
+  return <div>Hi {name}. You are a boob!!!</div>;
 }
 
 export default App;
